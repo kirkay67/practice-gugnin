021a808a44ffa62e16399c397c2a75ca40cbb7fb
Gugnin Kirill <kirya.gugnin@bk.ru>
2026-03-24 10:22:00 +0300

    test: add load test and results

---
+++ load_test/script.js
@@ -0,0 +1,8 @@
+import hhtp from 'k6/http';
+import {sleep} from 'k6';
+
+export let options = {vus: 10, duration:'30s'};
+export default function (){
+    hhtp.get('http://localhost:8080/');
+    sleep(1);
+}
